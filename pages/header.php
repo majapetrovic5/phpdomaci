77eb4c3f5d6f453f97df18319a39a156d0200129
majapetrovic5 <76411291+majapetrovic5@users.noreply.github.com>
2022-01-14 14:44:09 +0100

    Sredjen link u Header-u

--- pages/header.php
+++ pages/header.php
@@ -1,7 +1,7 @@
 
 <div class='navbar_div'>
     <nav class="navbar navbar-light sticky-top bottom_sep" style="background-color:#91acbf;">
-    <a class="navbar-brand a_header mb-0 h1" href="#">
+    <a class="navbar-brand a_header mb-0 h1" href="./vakcine.php">
     <img src="./logofinal.png" alt="" width="39" height="42" class="d-inline-block align-text-top"> Meni</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText"
      aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
